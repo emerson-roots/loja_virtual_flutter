e764ce8af1363d30c09c6c45b0be16c48b5c3e15
Emerson Melo <emerson_sardinha@hotmail.com>
2023-02-28 17:54:04 -0300

    Aula 211 - Mostrando Lista de Categorias

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -1,24 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
-      future: FirebaseFirestore.instance.collection("products").get(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else {
-          return ListView(
-            children: const [
+        future: FirebaseFirestore.instance.collection("products").get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            var dividedTiles = ListTile.divideTiles(
+              tiles: snapshot.data!.docs.map((doc) {
+                return CategoryTileCustom(doc);
+              }).toList(),
+              color: Colors.grey[500],
+            ).toList();
 
-            ],
-          );
-        }
-      },
-    );
+            return ListView(
+              children: dividedTiles,
+            );
+          }
+        });
   }
 }
