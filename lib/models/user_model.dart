7dd0684eac49a65365deb09d3974f3262e68d577
Emerson Melo <emerson@t2s.com.br>
2024-07-03 15:25:02 -0300

    Aula 224 - Código para Criar Usuário

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,25 +1,59 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
-  // usuario atual
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  UserCredential? firebaseUser;
+  Map<String, dynamic> userData = Map();
 
   bool isLoading = false;
 
-  void signUp() {}
+  void signUp({required Map<String, dynamic> userData,
+    required String pass,
+    required VoidCallback onSuccess,
+    required VoidCallback onFail}) {
+    configuraIsLoading(true);
+    _auth
+        .createUserWithEmailAndPassword(
+      email: userData["email"],
+      password: pass,
+    )
+        .then((user) async {
+      firebaseUser = user;
+      await _saveUserData(userData);
 
-  void signIn()  async {
-    isLoading = true;
-    notifyListeners();
-
-    await Future.delayed(const Duration(seconds: 3));
-
-    isLoading = false;
-    notifyListeners();
+      onSuccess();
+      configuraIsLoading(false);
+    }).catchError((ex) {
+      onFail();
+      configuraIsLoading(false);
+    });
   }
 
-  void recoverPass() {}
+  void signIn() async {
+    configuraIsLoading(true);
+    await Future.delayed(const Duration(seconds: 3));
+    configuraIsLoading(false);
+  }
+
+  void recoverPass() {
+  }
 
   bool isLoggedIn() {
     return false;
+  }
+
+  void configuraIsLoading(bool isLoading) {
+    isLoading = isLoading;
+    notifyListeners();
+  }
+
+  Future<void> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await FirebaseFirestore.instance.collection("users").doc(
+        firebaseUser!.user!.uid).set(userData);
   }
 }
