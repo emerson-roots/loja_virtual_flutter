c60a358e9807e040777fd59517222d62549c2a80
Emerson Melo <emerson@t2s.com.br>
2024-07-15 18:54:00 -0300

    Aula 247 - Acompanhando o Status do Pedido

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -21,6 +21,11 @@ class OrderTile extends StatelessWidget {
               .doc(orderId)
               .snapshots(),
           builder: (context, snapshot) {
+            int status =
+                snapshot.hasData && snapshot.data!.get("status") != null
+                    ? snapshot.data!.get("status")
+                    : 1;
+
             if (!snapshot.hasData) {
               return CustomActivityIndicator();
             } else {
@@ -34,7 +39,35 @@ class OrderTile extends StatelessWidget {
                   const SizedBox(
                     height: 4.0,
                   ),
-                  Text(_buildProductsText(snapshot!.data!))
+                  Text(_buildProductsText(snapshot!.data!)),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(
+                    "Status do Pedido:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      _buildCircle("1", "Preparação", status, 1),
+                      Container(
+                        height: 1.0,
+                        width: 40.0,
+                        color: Colors.grey.shade500,
+                      ),
+                      _buildCircle("2", "Transporte", status, 2),
+                      Container(
+                        height: 1.0,
+                        width: 40.0,
+                        color: Colors.grey.shade500,
+                      ),
+                      _buildCircle("3", "Entrega", status, 3),
+                    ],
+                  )
                 ],
               );
             }
@@ -46,15 +79,57 @@ class OrderTile extends StatelessWidget {
 
   String _buildProductsText(DocumentSnapshot snapshot) {
     String text = "Descrição:\n";
-
     for (LinkedHashMap p in snapshot.get("products")) {
-      print(p);
       text +=
           "${p["quantity"]} x ${p["product"]["title"]} (R\$ ${p["product"]["price"].toStringAsFixed(2)})\n";
     }
 
     text += "Total: R\$ ${snapshot.get("totalPrice").toStringAsFixed(2)}";
-
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subtitle, int status, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey.shade500;
+      child = Text(
+        title,
+        style: const TextStyle(color: Colors.white),
+      );
+    } else if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = const Icon(
+        Icons.check,
+        color: Colors.white,
+      );
+    }
+
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subtitle)
+      ],
+    );
   }
 }
